802c12924ef09ddf92c616b256b04ff28fc59a97
uniflusantos <146308610+uniflusantos@users.noreply.github.com>
2024-10-24 19:12:52 -0300

    Update main.c

--- main.c
+++ main.c
@@ -22,8 +22,6 @@ typedef struct{
     Data entrada;
 }RegistroFull;
 
-
-
 typedef struct ELista
 {
     struct Elista * proximo;
@@ -35,6 +33,20 @@ typedef struct{
     int quantidade;
 }Lista;
 
+typedef struct EFila
+{
+    struct EFila * proximo;
+    Registro * Dados;    
+}EFila;
+
+typedef struct{
+    EFila * head;
+    EFila * tail;
+    int quantidade;
+}Fila;
+
+//Funções da lista
+
 Data* iniciarData(int aleatorio){
     Data* data = malloc(sizeof(Data));
     // time_t tempo;
@@ -44,14 +56,13 @@ Data* iniciarData(int aleatorio){
         data->ano = 2020 +rand()%5; //hora_local->tm_year;
         data->dia = rand()%30;
         data->mes = rand()%12;
-        return;
+        return NULL;
     }
     data->ano = 0; //hora_local->tm_year;
     data->dia = 0;
     data->mes = 0;
     return data;
 }
-
 
 Registro * iniciarRegistro(int dia, int mes, int ano){
     
@@ -67,6 +78,8 @@ Registro * iniciarRegistro(int dia, int mes, int ano){
     reg->rg = "";
     return reg;
 };
+
+//Funções da lista
 
 Lista * iniciarLista(){
     Lista * lista = malloc(sizeof(Lista));
@@ -93,6 +106,74 @@ void inserirLista(Lista * lista, Registro *valor){
     return;
 }
 
+Registro removerListaRG(Lista * lista, char* rg){
+    if(lista->quantidade == 0)return *iniciarRegistro(0,0,0);
+    ELista* anterior = NULL;
+    ELista* atual = lista->inicio;
+
+    while(atual !=NULL && strcmp(atual->Dados->rg, rg)!=0){
+        anterior = atual;
+        atual = atual->proximo;
+    }
+    if(atual == NULL)return *iniciarRegistro(0,0,0);
+    if(anterior == NULL){
+        lista->inicio = atual->proximo;
+    }
+    if(atual->proximo == NULL){
+        anterior->proximo = NULL;
+    }else{
+        anterior = atual->proximo;
+    }
+    free(atual);
+    lista->quantidade--;
+    
+}
+
+Registro removerListaNome(Lista * lista, char* nome){
+    if(lista->quantidade == 0)return *iniciarRegistro(0,0,0);
+    ELista* anterior = NULL;
+    ELista* atual = lista->inicio;
+
+    while(atual !=NULL && strcmp(atual->Dados->nome, nome)!=0){
+        anterior = atual;
+        atual = atual->proximo;
+    }
+    if(atual == NULL)return *iniciarRegistro(0,0,0);
+    if(anterior == NULL){
+        lista->inicio = atual->proximo;
+    }
+    if(atual->proximo == NULL){
+        anterior->proximo = NULL;
+    }else{
+        anterior = atual->proximo;
+    }
+    free(atual);
+    lista->quantidade--;
+    
+}
+
+Registro * procurarRegistroPeloNome(Lista* lista, char* nome){
+    ELista* atual = lista->inicio;
+    while(atual !=NULL && strcmp(atual->Dados->nome, nome)!=0){
+        atual = atual->proximo;
+    }
+    if(atual == NULL)return NULL;
+    return atual->Dados;
+    
+}
+
+Registro * procurarRegistroPeloRG(Lista* lista, char* rg){
+    ELista* atual = lista->inicio;
+    while(atual !=NULL && strcmp(atual->Dados->rg, rg)!=0){
+        atual = atual->proximo;
+    }
+    if(atual == NULL)return NULL;
+    return atual->Dados;
+    
+}
+
+//Prints 
+
 void printarData(Data * data){
     printf("Data: %02d/%02d/%04d \n",data->dia,data->mes,data->ano);
 }
@@ -102,7 +183,7 @@ void printarCadastro(Registro* registro){
     printf("Nome: %s\nIdade: %d\nRG: %s \n\n",registro->nome, registro->idade, registro->rg);
 }
 
-void mostrarListaCadastros(Lista* lista,char* rg){
+void mostrarListaCadastros(Lista* lista){
     ELista *inicio = lista->inicio;
     while(inicio != NULL){
         printarCadastro(inicio->Dados);
@@ -110,6 +191,8 @@ void mostrarListaCadastros(Lista* lista,char* rg){
     }
     printf("\n");
 }
+
+// Funções de load e save
 
 void limparArquivoBinario(const char *caminho) {
     FILE *file = fopen(caminho, "wb");
@@ -142,11 +225,11 @@ Lista * lerDados(){
     RegistroFull* atual = malloc(sizeof(RegistroFull));
     FILE *file = fopen("dados.bin", "rb");
     while (fread(atual, sizeof(RegistroFull), 1, file) == 1) {
-        Registro reg;
-        reg.nome = atual->nome;
-        reg.rg = atual->rg;
-        reg.idade = atual->idade;
-        reg.entrada = &atual->entrada;
+        Registro * reg = malloc(sizeof(Registro));
+        reg->nome = atual->nome;
+        reg->rg = atual->rg;
+        reg->idade = atual->idade;
+        reg->entrada = &(atual->entrada);
         inserirLista(lista,&reg);
         
     }
@@ -154,18 +237,87 @@ Lista * lerDados(){
     return lista;    
 } 
 
+//Funções da Fila
+
+EFila * iniciarEFila(){
+    EFila* celulaFila = malloc(sizeof(EFila));
+    celulaFila->Dados = iniciarRegistro(0,0,0);
+    celulaFila->proximo = NULL;
+    return celulaFila;
+}
+
+EFila * iniciarEFilaReg(Registro* registro){
+    EFila* celulaFila = malloc(sizeof(EFila));
+    celulaFila->Dados = registro;
+    celulaFila->proximo = NULL;
+    return celulaFila;
+}
+
+Fila * iniciarFila(){
+    Fila* fila = malloc(sizeof(Fila));
+    fila->head = NULL;
+    fila->tail = NULL;
+    fila->quantidade = 0;
+    return fila;
+}
+
+void inserirFila(Fila * fila,Registro* reg){
+    EFila * novo = iniciarEFila(reg);
+    if(fila->quantidade== 0){
+        fila->head = novo;
+    }else{
+        fila->tail->proximo = novo;
+    }
+    fila->tail = novo;
+    fila->quantidade++;    
+}
+
+Registro removerFila(Fila * fila){
+    if(fila->quantidade ==0)return *iniciarRegistro(0,0,0);
+    Registro valorRemocao = *fila->head->Dados;
+    EFila * remover = fila->head;
+    fila->head = fila->head->proximo;
+    free(remover);
+    fila->quantidade--;
+    return valorRemocao;
+}
+
+void mostrarFila(Fila * fila){
+    EFila * inicio = fila->head;
+    while(inicio != NULL){
+        printarCadastro(inicio->Dados);
+        inicio=inicio->proximo;
+    }
+    printf("\n\n\n\n");
+}
+
+
+
 int main(){
     
-    Lista* lista = iniciarLista();
-    Registro reg = {.entrada=iniciarData(0),.idade=19,.nome="Giovanni",.rg="123"};
-    inserirLista(lista,&reg);
-    inserirLista(lista,&reg);
-    inserirLista(lista,&reg);
-    inserirLista(lista,&reg);
-    mostrarListaCadastros(lista,"123");
-    salvarDados(lista);
+    // Lista* lista = lerDados();
+    // Lista * lista= iniciarLista();
+    // Registro reg = {.entrada=iniciarData(0),.idade=19,.nome="Giovanni",.rg="123"};
+    // Registro reg2 = {.entrada=iniciarData(0),.idade=19,.nome="Julia",.rg="321"}; 
+
+    // inserirLista(lista,&reg2);
+    // inserirLista(lista,&reg);
+    // // mostrarListaCadastros(lista);
+    // // printf("Removido nome Giovanni \n\n\n\n\n");
+    // // removerListaNome(lista,"Giovanni");
     
-    while(1){
+    // mostrarListaCadastros(lista);
+    // // printarCadastro(procurarRegistroPeloNome(lista,"Giovanni"));
+    // // printarCadastro(procurarRegistroPeloRG(lista,"321"));
+
+    // // Registro * regAtual = procurarRegistroPeloRG(lista,"321");
+    // // regAtual->entrada->ano= 2024;
+    // // printarCadastro(procurarRegistroPeloRG(lista,"321"));
+
+    // salvarDados(lista);
+    
+    
+    Lwhile(1){
     	int opcao;
 	printf("1 - Opcoes de cadastro\n");
 	printf("2 - Atendimento\n");
@@ -185,7 +337,18 @@ int main(){
 		printf("5 - Remover paciente\n");
 		printf("Digite qual opcao deseja utilizar\n");
 		scanf("%d", &opcao_1);
+			
+			if(opcao_1 == 1){
+				inserirLista(lista, &reg);
+			}
+			else if(opcao_1 == 2){
+				printarCadastro(&reg);
+			}
+			else if(opcao_1 == 3){
+				mostrarListaCadastrados(lista, "123");
+			}	
 		}
+
 	if(opcao == 2){
 		int opcao_2;
 		printf("1 - Enfileirar paciente na lista de atendimento\n");
@@ -218,11 +381,12 @@ int main(){
 		printf("Disciplina: Estrutura de dados\n");
 		printf("Data: \n");
 	}
-    	
+}
     Lista* lista2 = lerDados();
     mostrarListaCadastros(lista2,"123");
     
 
 
     return 1;
+	
 }
