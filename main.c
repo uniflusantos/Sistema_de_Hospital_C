7f63534388f6b53c263a5a232b37224b3c17a1b5
Giovanni Chahin Morassi <giovannimorassi13511@gmail.com>
2024-11-02 15:39:20 -0300

    Acrescentando a arvore binaria
    Agora temos a funcionalidade de criar uma arvore de uma lista ordenando por algumas possibilidades, Ano, Mes, Dia de cadastro e a idade

--- main.c
+++ main.c
@@ -43,6 +43,45 @@ typedef struct {
     int quantidade;
 } Fila;
 
+typedef struct CelulaStack{
+    struct CelulaStack* proximo;
+    int valor;
+}CelulaStack;
+
+typedef struct{
+    CelulaStack *topo;
+    int qtde;
+}StackInt;
+
+typedef struct VerticeArvore{
+	Registro *valor;
+	struct VerticeArvore* esq;
+	struct VerticeArvore* dir;
+	struct VerticeArvore* pai;
+} VerticeArvore;
+
+typedef struct ArvoreBinaria{
+	VerticeArvore* raiz;
+	int qtde;
+    void (*inserirArvore[4])(struct ArvoreBinaria*, Registro*);
+    int (*remover)(struct ArvoreBinaria* ,VerticeArvore*);
+} ArvoreBinaria;
+
+
+StackInt * iniciarStack(){
+    StackInt* pilha = malloc(sizeof(StackInt));
+    pilha->qtde=0;
+    pilha->topo= NULL;
+    return pilha;
+}
+
+CelulaStack* instanciar_celula(CelulaStack* proximo,int valor){
+    CelulaStack * cell = malloc(sizeof(CelulaStack));
+    cell->valor= valor;
+    cell->proximo = proximo;
+    return cell;
+}
+
 Data* iniciarData(int aleatorio) {
     Data* data = malloc(sizeof(Data));
     if (aleatorio) {
@@ -87,6 +126,43 @@ ELista *iniciarELista(Registro *registro) {
     celula->Dados = registro;
     celula->proximo = NULL;
     return celula;
+}
+
+VerticeArvore *iniciarVerticeArvoreBinaria(Registro* registrado){
+	VerticeArvore* novo = malloc(sizeof(VerticeArvore));
+	novo->dir = NULL;
+	novo->esq = NULL;
+	novo->pai = NULL;
+	novo->valor = registrado;
+	
+	return novo;
+}
+
+
+
+
+void push_stack(StackInt * pilha, int valor){
+    pilha->topo = instanciar_celula(pilha->topo,valor);
+    pilha->qtde++;
+}
+
+int pop_stack(StackInt * pilha){
+    if (pilha->qtde==0)return -1;
+    CelulaStack* deletando = pilha->topo;
+    int valor = deletando->valor;
+    pilha->topo=pilha->topo->proximo;
+    free(deletando);
+    pilha->qtde--;
+    return valor;
+}
+
+void show_stack(StackInt* pilha){
+        CelulaStack *topo = pilha->topo;
+    while(topo!=NULL){
+        printf("%d ",topo->valor);
+        topo = topo->proximo;
+    }
+    printf("\n");
 }
 
 void inserirLista(Lista *lista, Registro *valor) {
@@ -210,16 +286,17 @@ Lista *lerDados() {
     FILE *file = fopen("dados.bin", "rb");
     if (file == NULL) {
         printf("Erro ao abrir o arquivo para leitura!\n");
+        file = fopen("dados.bin", "w");
+        fclose(file);
         return lista;
     }
     while (fread(&atual, sizeof(RegistroFull), 1, file) == 1) {
-    Registro *reg = iniciarRegistro(atual.entrada.dia, atual.entrada.mes, atual.entrada.ano);
-    strcpy(reg->nome, atual.nome);
-    strcpy(reg->rg, atual.rg);
-    reg->idade = atual.idade;  // Aqui está a correção
-    inserirLista(lista, reg);
+        Registro *reg = iniciarRegistro(atual.entrada.dia, atual.entrada.mes, atual.entrada.ano);
+        strcpy(reg->nome, atual.nome);
+        strcpy(reg->rg, atual.rg);
+        reg->idade = atual.idade;
+        inserirLista(lista, reg);
     }
-
     fclose(file);
     return lista;    
 } 
@@ -279,6 +356,204 @@ void mostrarFila(Fila *fila) {
     printf("\n");
 }
 
+void printarArvoreEmOrdemCrescente(VerticeArvore *raiz) {
+    if(raiz != NULL){
+        printarArvoreEmOrdemCrescente(raiz->esq);
+        printarCadastro(raiz->valor);
+        printarArvoreEmOrdemCrescente(raiz->dir);
+    }
+}
+
+void inserirArvoreIdade(ArvoreBinaria* arvore, Registro* valor){
+
+	VerticeArvore* novo = iniciarVerticeArvoreBinaria(valor);
+    if(arvore->qtde==0){
+        arvore->raiz=novo;
+    }   
+    else{
+        VerticeArvore * anterior = NULL;
+        VerticeArvore * atual = arvore->raiz;
+        while( atual!= NULL){
+            if(valor->idade > atual->valor->idade){
+                anterior = atual;
+                atual = atual->dir;
+            }else{
+                anterior = atual;
+                atual = atual->esq;
+            }
+        }
+        if(valor->idade > anterior->valor->idade){
+            anterior->dir=novo;
+            novo->pai=anterior;
+        }else{
+            anterior->esq = novo;
+            novo->pai= anterior;
+        }
+    }
+    arvore->qtde++;
+}
+
+void inserirArvoreAno(ArvoreBinaria* arvore, Registro* valor){
+
+	VerticeArvore* novo = iniciarVerticeArvoreBinaria(valor);
+    if(arvore->qtde==0){
+        arvore->raiz=novo;
+    }   
+    else{
+        VerticeArvore * anterior = NULL;
+        VerticeArvore * atual = arvore->raiz;
+        while( atual!= NULL){
+            if(valor->entrada->ano > atual->valor->entrada->ano){
+                anterior = atual;
+                atual = atual->dir;
+            }else{
+                anterior = atual;
+                atual = atual->esq;
+            }
+        }
+        if(valor->entrada->ano > anterior->valor->entrada->ano){
+            anterior->dir=novo;
+            novo->pai=anterior;
+        }else{
+            anterior->esq = novo;
+            novo->pai= anterior;
+        }
+    }
+    arvore->qtde++;
+}
+
+void inserirArvoreMes(ArvoreBinaria* arvore, Registro* valor){
+
+	VerticeArvore* novo = iniciarVerticeArvoreBinaria(valor);
+    if(arvore->qtde==0){
+        arvore->raiz=novo;
+    }   
+    else{
+        VerticeArvore * anterior = NULL;
+        VerticeArvore * atual = arvore->raiz;
+        while( atual!= NULL){
+            if(valor->entrada->mes > atual->valor->entrada->mes){
+                anterior = atual;
+                atual = atual->dir;
+            }else{
+                anterior = atual;
+                atual = atual->esq;
+            }
+        }
+        if(valor->entrada->mes > anterior->valor->entrada->mes){
+            anterior->dir=novo;
+            novo->pai=anterior;
+        }else{
+            anterior->esq = novo;
+            novo->pai= anterior;
+        }
+    }
+    arvore->qtde++;
+}
+
+void inserirArvoreDia(ArvoreBinaria* arvore, Registro* valor){
+
+	VerticeArvore* novo = iniciarVerticeArvoreBinaria(valor);
+    if(arvore->qtde==0){
+        arvore->raiz=novo;
+    }   
+    else{
+        VerticeArvore * anterior = NULL;
+        VerticeArvore * atual = arvore->raiz;
+        while( atual!= NULL){
+            if(valor->entrada->dia > atual->valor->entrada->dia){
+                anterior = atual;
+                atual = atual->dir;
+            }else{
+                anterior = atual;
+                atual = atual->esq;
+            }
+        }
+        if(valor->entrada->dia > anterior->valor->entrada->dia){
+            anterior->dir=novo;
+            novo->pai=anterior;
+        }else{
+            anterior->esq = novo;
+            novo->pai= anterior;
+        }
+    }
+    arvore->qtde++;
+}
+
+
+int removerVerticeArvoreBinaria(ArvoreBinaria* arvore, VerticeArvore* vertice) {
+    int filhos = (vertice->dir? 1: 0)+(vertice->esq? 1: 0);
+    if (filhos ==0) {
+        if (vertice->pai) {
+            if (vertice->pai->esq == vertice) {
+                vertice->pai->esq = NULL;
+            } else {
+                vertice->pai->dir = NULL;
+            }
+        } else {
+            arvore->raiz = NULL; // Remoção da raiz se for o único nó
+        }
+        free(vertice);
+    }
+    // Caso 2: VerticeArvore tem um único filho
+    else if (filhos == 1) {
+        VerticeArvore* filho = (vertice->esq != NULL) ? vertice->esq : vertice->dir;
+        
+        if (vertice->pai) {
+            if (vertice->pai->esq == vertice) {
+                vertice->pai->esq = filho;
+            } else {
+                vertice->pai->dir = filho;
+            }
+        } else {
+            arvore->raiz = filho; // Atualiza a raiz se o nó removido for a raiz
+        }
+        filho->pai = vertice->pai;
+        free(vertice);
+    }
+    // Caso 3: VerticeArvore tem dois filhos
+    else {
+        // Encontra o sucessor in-ordem (menor valor na subárvore direita)
+        VerticeArvore* sucessor = vertice->esq;
+        while (sucessor->dir != NULL) {
+            sucessor = sucessor->dir;
+        }
+
+        // Copia o valor do sucessor para o nó a ser removido
+        vertice->valor = sucessor->valor;
+
+        // Remove o sucessor que agora é duplicado
+        removerVerticeArvoreBinaria(arvore, sucessor);
+    }
+
+    arvore->qtde--;
+    return 1; // Indica sucesso
+}
+
+ArvoreBinaria *iniciarArvoreBinaria(){
+	ArvoreBinaria* arvore = malloc(sizeof(ArvoreBinaria));
+	arvore->raiz = NULL;
+	arvore->qtde = 0;
+    arvore->inserirArvore[0] = inserirArvoreIdade;
+    arvore->inserirArvore[1] = inserirArvoreDia;
+    arvore->inserirArvore[2] = inserirArvoreMes;
+    arvore->inserirArvore[3] = inserirArvoreAno;
+    arvore->remover = removerVerticeArvoreBinaria;
+	return arvore;
+}
+
+ArvoreBinaria* criarArvoreAPartirDeUmaLista(Lista* lista,int variavelAnalizada){
+    ELista* primeiro = lista->inicio;
+    ArvoreBinaria* arvore = iniciarArvoreBinaria();
+    while (primeiro != NULL) {
+        arvore->inserirArvore[variavelAnalizada](arvore,primeiro->Dados);
+        primeiro = primeiro->proximo;
+    }
+    return arvore;
+}
+
+
+
 void opcoesCadastro(Lista* lista) {
     int opcao_1;
     printf("1 - Cadastrar novo paciente\n");
@@ -336,41 +611,17 @@ void opcoesCadastro(Lista* lista) {
     }
 }
 
-void opcao2(Fila *fila, Lista *lista) {
+void opcao2(Fila *fila, Registro *reg){
     int opcao_2;
     printf("1 - Inserir paciente na fila de atendimento\n");
     printf("2 - Remover paciente da fila de atendimento\n");
     printf("3 - Mostrar fila de atendimento\n");
     printf("Digite qual opcao deseja utilizar\n");
     scanf("%d", &opcao_2);
-    
-    if (opcao_2 == 1) {
-        char rg[STR_LIM];
-        printf("Digite o RG do paciente que deseja enfileirar: \n");
-        scanf("%s", rg);
-        Registro *registro = procurarRegistroPeloRG(lista, rg);
-        if (registro) {
-            inserirFila(fila, registro);
-        } else {
-            printf("Paciente com RG %s não encontrado.\n", rg);
-        }
-    } else if (opcao_2 == 2) {
-        char rg[STR_LIM];
-        printf("Digite o RG do paciente que deseja desenfileirar: \n");
-        scanf("%s", rg);
-        Registro *registro = procurarRegistroPeloRG(lista, rg);
-        if(registro){
-            removerFila(fila);
-        }
-        else{
-            printf("Paciente com RG %s não encontrado.\n ", rg);
-        }
-        removerFila(fila);
-    } else if (opcao_2 == 3) {
-        mostrarFila(fila);
+    if(opcao_2 == 1){
+        inserirFila(fila, reg);
     }
 }
-
 
 void opcao5(Lista *lista){
     int opcao_5;
@@ -388,17 +639,17 @@ void opcao5(Lista *lista){
 
 void opcao6(){
     printf("Programa de: \n");
-    printf("Lucas Roberto Boccia dos Santos (22.123.012-1)");
-    printf("Giovanni Chahin Morassi (22.123.025-3)");
-    printf("Ciclo: 4");
-    printf("Curso: Ciencias da Computacao");
-    printf("Disciplina: Estrutura de Dados");
+    printf("Lucas Roberto Boccia dos Santos (22.123.012-1)\n");
+    printf("Giovanni Chahin Morassi (22.123.025-3)\n");
+    printf("Ciclo: 4\n");
+    printf("Curso: Ciencias da Computacao\n");
+    printf("Disciplina: Estrutura de Dados\n");
 }
 
 int main() {
-    Lista* lista2 = lerDados();
-    mostrarListaCadastros(lista2);
-    Fila *queue = iniciarFila();
+    Lista* lista = lerDados();
+    mostrarListaCadastros(lista);
+    
     while (1) {
         int opcao;
         printf("Givas Health Care System\n");
@@ -414,15 +665,11 @@ int main() {
         scanf("%d", &opcao);
 
         if (opcao == 1) {
-            opcoesCadastro(lista2);
-        }
-
-        if (opcao == 2){
-            opcao2(queue, lista2);
+            opcoesCadastro(lista);
         }
 
         if (opcao == 5){
-            opcao5(lista2);
+            opcao5(lista);
         }
         
         if (opcao == 6){
@@ -430,11 +677,13 @@ int main() {
         }
         
         if (opcao == 7) {
+            salvarDados(lista);
             break;
         }
     }
     
     // Liberar memória
-    free(lista2);
+    free(lista);
+
     return 0;
 }
