9b2e2572ac718a6f188e74220111a01e86ea86b0
uniflusantos <146308610+uniflusantos@users.noreply.github.com>
2024-11-12 16:02:26 -0300

    Update Funcoes.h

--- Funcoes.h
+++ Funcoes.h
@@ -6,9 +6,9 @@
 
 void limpa_buffer();
 void opcoesCadastro(Lista* lista);
-void opcao2(Fila *fila, Lista *lista);
+void opcao2(Fila *fila, Lista *lista, StackRegistro *stack, StackInt *pilha);
 void opcao3(Lista *lista);
-void opcao4();
+void opcao4(Fila *fila, StackRegistro *pilhaRegistro, StackInt *pilhaOperacao);
 void opcao5(Lista *lista);
 void opcao6();
 
